6a00844bd24ab8b1a435d1a8e514b21b00fcde92
jin <jinpark1504@gmail.com>
2019-11-30 16:19:33 +0000

    MyComponent

---
+++ src/MyComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class MyComponent extends Component {
+    render() {
+        return (
+            <div>New Component</div>
+        )
+    }
+}
+
+export default MyComponent;
